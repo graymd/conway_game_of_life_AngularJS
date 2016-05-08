1dd149ab8f18bb6813008b059a767b5619fb49cd
Malachi Gray <graymd.cfi@gmail.com>
2016-05-08 18:44:37 -0400

    udate game_game_of_life.js to be angular app - update naming

--- js/conway_game_of_life.js
+++ js/conway_game_of_life.js
@@ -1,228 +1,174 @@
-// Calculate number of cells to be displayed
-// Set random alive cells in a 2D array
-// Perform logic to figure out what the next set of alive cells should be
-// Set next array to current array to display
-// continuously perform this logic
+var conwayGameOfLifeApp = angular.module("conwayGameOfLifeApp", []);
 
-var width = 10;
-var height = 10;
-var gameOn = true;
+var conwayGameOfLifeCtrl = conwayGameOfLifeApp.controller('conwayGameOfLifeCtrl', function($scope, $timeout){
+  $scope.width = 10;
+  $scope.height = 10;
+  $scope.nodes = createCurrentArray($scope.width, $scope.height);
 
+  $scope.nextIteration = function() {
+    nextIteration($scope.width, $scope.height, $scope.nodes);
+  } 
+    
+  $scope.updateBoardSize = function() {
+    var intWidth = parseInt($scope.gameInputs.width);
+    var intHeight = parseInt($scope.gameInputs.height);
+    updatedBoard = createCurrentArray(intWidth, intHeight);
+    $scope.nodes = updatedBoard;
+    $scope.width = intWidth;
+    $scope.height = intHeight;
+  }
 
-var startingArray = new Array(width).fill(0);
-for (var i = 0; i < startingArray.length; i++) {
-  startingArray[i] = new Array(width).fill(0);
-}
+  function createCurrentArray(width, height){
+    var currentArray = new Array(width).fill(0);
+    for (var i = 0; i < currentArray.length; i++) {
+      currentArray[i] = new Array(height).fill(0);
+    } 
 
-var nextArray = new Array(width).fill(0);
-for (var i = 0; i < nextArray.length; i++) {
-  nextArray[i] = new Array(width).fill(0);
-}
-
-
-// console.log(startingArray);
-function currentArray(){
-  currentArray = startingArray;
-  currentArray[1][4] = 1;
-  currentArray[2][4] = 1;
-  currentArray[3][4] = 1;
-}
-
-// currentArray[0][1] = 1;
-
-// currentArray[4][1] = 1;
-
-
-function playGame(){
-  var sleep = require('sleep');
-  while (gameOn){
-    sleep.sleep(1);
-    var counter = 0;
     for (var i = 0; i < currentArray.length; i++){
       var innerArray = currentArray[i];
       for (var j = 0; j < innerArray.length; j++){
-        // console.log(innerArray[j]);
-        // console.log(updatePoint(i, j, innerArray[j]));
-        newAliveOrDead = updatePoint(i, j, innerArray[j]);
-        nextArray[i][j] = newAliveOrDead;
-        counter += newAliveOrDead;
-        
-        //Method to updatePointAlive() || updatePointDead();
+        currentArray[i][j] = randomAliveDead();
       }
-
     }
+    return currentArray;
+  }
 
-    console.log(nextArray);
-    currentArray = nextArray;
+  function randomAliveDead(){
+    return Math.round(Math.random());
+  }
+
+  function createNextArray(width, height){
     var nextArray = new Array(width).fill(0);
     for (var i = 0; i < nextArray.length; i++) {
-      nextArray[i] = new Array(width).fill(0);
+      nextArray[i] = new Array(height).fill(0);
     }
-    checkGameOn(counter);
+   return nextArray;
   }
-}
 
-
-function checkGameOn(counter){
-  if (counter === 0){
-    gameOn = false;
+  function nextIteration(width, height, currentState){
+    currentArray = currentState;
+    nextArray = createNextArray(width, height);
+      for (var i = 0; i < currentArray.length; i++){
+        var innerArray = currentArray[i];
+        for (var j = 0; j < innerArray.length; j++){
+          newAliveOrDead = updatePoint(i, j, innerArray[j]);
+          nextArray[i][j] = newAliveOrDead;
+        }
+      }
+      currentArray = nextArray;
+      $scope.nodes = currentArray;
   }
-}
 
-function updatePoint(x, y, status){
+  // function checkGameOn(counter){
+  //   if (counter === 0){
+  //     gameOn = false;
+  //   }
+  // }
 
-// update neighborArray with the points.  count total number in that array
-//return a 1 or 0 for that point and let the loop above update the point
-  var neighborAliveCount = 0;
-  var neighborArray = [];
-  neighborArray[0] = checkNorth(x, y);
-  neighborArray[1] = checkNorthEast(x, y);
-  neighborArray[2] = checkEast(x, y);
-  neighborArray[3] = checkSouthEast(x, y);
-  neighborArray[4] = checkSouth(x, y);
-  neighborArray[5] = checkSouthWest(x, y);
-  neighborArray[6] = checkWest(x, y);
-  neighborArray[7] = checkNorthWest(x, y);
-  neighborAliveCount = neighborArray.reduce(add, 0);
-  // console.log(neighborAliveCount);
-  return checkAgainstRules(status, neighborAliveCount);
+  function updatePoint(x, y, status){
 
-}
-
-function checkAgainstRules(status, neighborAliveCount){
-  if (status === 1){
-    return checkLiveRules(neighborAliveCount);
-  } else {
-    return checkDeadRules(neighborAliveCount);
+    var neighborAliveCount = 0;
+    var neighborArray = [];
+    neighborArray[0] = checkNorth(x, y);
+    neighborArray[1] = checkNorthEast(x, y);
+    neighborArray[2] = checkEast(x, y);
+    neighborArray[3] = checkSouthEast(x, y);
+    neighborArray[4] = checkSouth(x, y);
+    neighborArray[5] = checkSouthWest(x, y);
+    neighborArray[6] = checkWest(x, y);
+    neighborArray[7] = checkNorthWest(x, y);
+    neighborAliveCount = neighborArray.reduce(add, 0);
+    return checkAgainstRules(status, neighborAliveCount);
   }
-}
 
-function checkLiveRules(neighborAliveCount){
-  if (neighborAliveCount < 2) {
-    return 0;
-  } else if (neighborAliveCount <= 3){
-    return 1;
-  } else {
-    return 0;
+  function checkAgainstRules(status, neighborAliveCount){
+    if (status === 1){
+      return checkLiveRules(neighborAliveCount);
+    } else {
+      return checkDeadRules(neighborAliveCount);
+    }
   }
-}
 
-function checkDeadRules(neighborAliveCount){
-  if (neighborAliveCount === 3){
-    return 1;
-  } else {
-    return 0;
+  function checkLiveRules(neighborAliveCount){
+    if (neighborAliveCount < 2) {
+      return 0;
+    } else if (neighborAliveCount <= 3){
+      return 1;
+    } else {
+      return 0;
+    }
   }
-}
 
-function add(num1, num2){
-  return num1 + num2;
-}
-
-
-
-function checkNorth(x, y) {
-  if (typeof(currentArray[x + 1]) == 'undefined' || typeof(currentArray[x + 1][y]) == 'undefined'){
-    return 0;
+  function checkDeadRules(neighborAliveCount){
+    return neighborAliveCount === 3 ? 1 : 0;
   }
-  var point = currentArray[x + 1][y];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
+
+  function add(num1, num2){
+    return num1 + num2;
   }
-}
 
-function checkNorthEast(x, y) {
-  if (typeof(currentArray[x + 1]) == 'undefined' || typeof(currentArray[x + 1][y + 1]) == 'undefined'){
-    return 0;
+  function checkNorth(x, y) {
+    if (typeof(currentArray[x + 1]) == 'undefined' || typeof(currentArray[x + 1][y]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x + 1][y];
+    return point === 1 ? 1 : 0;
   }
-  var point = currentArray[x + 1][y + 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
+
+  function checkNorthEast(x, y) {
+    if (typeof(currentArray[x + 1]) == 'undefined' || typeof(currentArray[x + 1][y + 1]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x + 1][y + 1];
+    return point === 1 ? 1 : 0;
   }
-}
 
-
-function checkEast(x, y) {
-  if (typeof(currentArray[x]) == 'undefined' || typeof(currentArray[x][y + 1]) == 'undefined'){
-    return 0;
+  function checkEast(x, y) {
+    if (typeof(currentArray[x]) == 'undefined' || typeof(currentArray[x][y + 1]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x][y + 1];
+    return point === 1 ? 1 : 0;
   }
-  var point = currentArray[x][y + 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
+
+  function checkSouthEast(x, y) {
+    if (typeof(currentArray[x - 1]) == 'undefined' || typeof(currentArray[x - 1][y + 1]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x - 1][y + 1];
+    return point === 1 ? 1 : 0;
   }
-}
 
-function checkSouthEast(x, y) {
-  if (typeof(currentArray[x - 1]) == 'undefined' || typeof(currentArray[x - 1][y + 1]) == 'undefined'){
-    return 0;
+  function checkSouth(x, y) {
+    if (typeof(currentArray[x - 1]) == 'undefined'  || typeof(currentArray[x - 1][y]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x - 1][y];
+    return point === 1 ? 1 : 0;
   }
-  var point = currentArray[x - 1][y + 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
+
+  function checkSouthWest(x, y) {
+    if (typeof(currentArray[x - 1]) == 'undefined' || typeof(currentArray[x - 1][y - 1]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x - 1][y - 1];
+    return point === 1 ? 1 : 0;
   }
-}
 
-function checkSouth(x, y) {
-  if (typeof(currentArray[x - 1]) == 'undefined'  || typeof(currentArray[x - 1][y]) == 'undefined'){
-    return 0;
+  function checkWest(x, y) {
+    if (typeof(currentArray[x]) == 'undefined' || typeof(currentArray[x][y - 1]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x][y - 1];
+    return point === 1 ? 1 : 0;
   }
-  var point = currentArray[x - 1][y];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
+
+  function checkNorthWest(x, y) {
+    if (typeof(currentArray[x + 1]) == 'undefined' || typeof(currentArray[x + 1][y - 1]) == 'undefined'){
+      return 0;
+    }
+    var point = currentArray[x + 1][y - 1];
+    return point === 1 ? 1 : 0;
   }
-}
 
-function checkSouthWest(x, y) {
-  if (typeof(currentArray[x - 1]) == 'undefined' || typeof(currentArray[x - 1][y - 1]) == 'undefined'){
-    return 0;
-  }
-  var point = currentArray[x - 1][y - 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-function checkWest(x, y) {
-  if (typeof(currentArray[x]) == 'undefined' || typeof(currentArray[x][y - 1]) == 'undefined'){
-    return 0;
-  }
-  var point = currentArray[x][y - 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-function checkNorthWest(x, y) {
-  if (typeof(currentArray[x + 1]) == 'undefined' || typeof(currentArray[x + 1][y - 1]) == 'undefined'){
-    return 0;
-  }
-  var point = currentArray[x + 1][y - 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-
-
-
-
-
-
-
-
+});
