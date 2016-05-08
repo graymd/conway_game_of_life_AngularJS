333422373743900f5a4097017cc4ec5ae613d7ac
Malachi Gray <graymd.cfi@gmail.com>
2016-05-08 00:48:54 -0400

    some cleanup of angular controller

--- js/test.js
+++ js/test.js
@@ -1,7 +1,6 @@
 var myApp = angular.module("myApp", []);
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $timeout){
-  // $scope.nodes = playGame();
   $scope.width = 10;
   $scope.height = 10;
   $scope.nodes = createCurrentArray($scope.width, $scope.height);
@@ -14,13 +13,10 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $timeout){
     var intWidth = parseInt($scope.gameInputs.width);
     var intHeight = parseInt($scope.gameInputs.height);
     updatedBoard = createCurrentArray(intWidth, intHeight);
-    console.log(updatedBoard);
     $scope.nodes = updatedBoard;
     $scope.width = intWidth;
     $scope.height = intHeight;
   }
-
-// var gameOn = true;
 
 function createCurrentArray(width, height){
   var currentArray = new Array(width).fill(0);
@@ -34,7 +30,6 @@ function createCurrentArray(width, height){
       currentArray[i][j] = randomAliveDead();
     }
   }
-
   return currentArray;
 }
 
@@ -45,49 +40,33 @@ function randomAliveDead(){
 function createNextArray(width, height){
   var nextArray = new Array(width).fill(0);
   for (var i = 0; i < nextArray.length; i++) {
-  nextArray[i] = new Array(width).fill(0);
- }
+    nextArray[i] = new Array(height).fill(0);
+  }
  return nextArray;
 }
 
 function nextIteration(width, height, currentState){
   currentArray = currentState;
   nextArray = createNextArray(width, height);
-  // while (gameOn){
-    // var counter = 0;
     for (var i = 0; i < currentArray.length; i++){
       var innerArray = currentArray[i];
       for (var j = 0; j < innerArray.length; j++){
-        // console.log(innerArray[j]);
-        // console.log(updatePoint(i, j, innerArray[j]));
         newAliveOrDead = updatePoint(i, j, innerArray[j]);
         nextArray[i][j] = newAliveOrDead;
-        // counter += newAliveOrDead;
-        
-        //Method to updatePointAlive() || updatePointDead();
       }
-
     }
-
-    // console.log(nextArray);
     currentArray = nextArray;
-    nextArray = createNextArray(width, height);
     $scope.nodes = currentArray;
-    // checkGameOn(counter); 
-  // }
 }
 
-
-function checkGameOn(counter){
-  if (counter === 0){
-    gameOn = false;
-  }
-}
+// function checkGameOn(counter){
+//   if (counter === 0){
+//     gameOn = false;
+//   }
+// }
 
 function updatePoint(x, y, status){
 
-// update neighborArray with the points.  count total number in that array
-//return a 1 or 0 for that point and let the loop above update the point
   var neighborAliveCount = 0;
   var neighborArray = [];
   neighborArray[0] = checkNorth(x, y);
@@ -99,9 +78,7 @@ function updatePoint(x, y, status){
   neighborArray[6] = checkWest(x, y);
   neighborArray[7] = checkNorthWest(x, y);
   neighborAliveCount = neighborArray.reduce(add, 0);
-  // console.log(neighborAliveCount);
   return checkAgainstRules(status, neighborAliveCount);
-
 }
 
 function checkAgainstRules(status, neighborAliveCount){
@@ -123,29 +100,19 @@ function checkLiveRules(neighborAliveCount){
 }
 
 function checkDeadRules(neighborAliveCount){
-  if (neighborAliveCount === 3){
-    return 1;
-  } else {
-    return 0;
-  }
+  return neighborAliveCount === 3 ? 1 : 0;
 }
 
 function add(num1, num2){
   return num1 + num2;
 }
 
-
-
 function checkNorth(x, y) {
   if (typeof(currentArray[x + 1]) == 'undefined' || typeof(currentArray[x + 1][y]) == 'undefined'){
     return 0;
   }
   var point = currentArray[x + 1][y];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
 
 function checkNorthEast(x, y) {
@@ -153,24 +120,15 @@ function checkNorthEast(x, y) {
     return 0;
   }
   var point = currentArray[x + 1][y + 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
-
 
 function checkEast(x, y) {
   if (typeof(currentArray[x]) == 'undefined' || typeof(currentArray[x][y + 1]) == 'undefined'){
     return 0;
   }
   var point = currentArray[x][y + 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
 
 function checkSouthEast(x, y) {
@@ -178,11 +136,7 @@ function checkSouthEast(x, y) {
     return 0;
   }
   var point = currentArray[x - 1][y + 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
 
 function checkSouth(x, y) {
@@ -190,11 +144,7 @@ function checkSouth(x, y) {
     return 0;
   }
   var point = currentArray[x - 1][y];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
 
 function checkSouthWest(x, y) {
@@ -202,11 +152,7 @@ function checkSouthWest(x, y) {
     return 0;
   }
   var point = currentArray[x - 1][y - 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
 
 function checkWest(x, y) {
@@ -214,11 +160,7 @@ function checkWest(x, y) {
     return 0;
   }
   var point = currentArray[x][y - 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
 
 function checkNorthWest(x, y) {
@@ -226,10 +168,7 @@ function checkNorthWest(x, y) {
     return 0;
   }
   var point = currentArray[x + 1][y - 1];
-  if (point === 1) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return point === 1 ? 1 : 0;
 }
+
 });
